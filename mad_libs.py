ba0eb5ea124d4a88dc4bc4be8f54ec5473011cb7
natcua <98064725+natcua@users.noreply.github.com>
2022-02-23 18:23:35 -0500

    Create mad_libs.py

---
+++ mad_libs.py
@@ -0,0 +1,28 @@
+#https://www.mikedane.com/programming-languages/python/building-a-mad-libs-game/
+#https://data-flair.training/blogs/python-mad-libs-generator-game/
+#create a story
+print('Story 1:')
+animals= input('enter a animal name : ')
+profession = input('enter a profession name: ')
+cloth = input('enter a piece of cloth name: ')
+things = input('enter a thing name: ')
+name= input('enter a name: ')
+place = input('enter a place name: ')
+verb = input('enter a verb in ing form: ')
+food = input('food name: ')
+
+print('say ' + food + ', the photographer said as the camera flashed! ' + name + ' and I had gone to ' + place +' to get our photos taken on my birthday. The first photo we really wanted was a picture of us dressed as ' + animals + ' pretending to be a ' + profession + '. when we saw the second photo, it was exactly what I wanted. We both looked like ' + things + ' wearing ' + cloth + ' and ' + verb + ' --exactly what I had in mind!!')
+
+print('  ')
+print('Story 2')
+adjactive = input('enter adjective : ')
+color = input('enter a color name : ')
+thing = input('enter a thing name :')
+place2 = input('enter a place name : ')
+person= input('enter a person name : ')
+adjactive1 = input('enter a adjactive : ')
+insect= input('enter a insect name : ')
+food2 = input('enter a food name : ')
+verb2 = input('enter a verb name : ')
+
+print('Last night I dreamed I was a ' +adjactive+ ' butterfly with ' + color+ ' splocthes that looked like '+thing+ ' .I flew to ' + place2+ ' with my bestfriend and '+person+ ' who was a '+adjactive1+ ' ' +insect +' .We ate some ' +food2+ ' when we got there and then decided to '+verb+ ' and the dream ended when I said-- lets ' +verb2+ '.')
