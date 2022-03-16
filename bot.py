d1c4f6794d702d39060e41e156a2d133669b1724
Nathalie <98064725+natcua@users.noreply.github.com>
2022-03-16 18:25:52 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
 #https://www.youtube.com/watch?v=SPTfmiYiuok
 
-#Step 1 - create bot (https://discord.com/developers/applications)
+#1 - create bot (https://discord.com/developers/applications)
   #Name: Test1 Bot
