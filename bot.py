0f771fb9d0b05568dbe69060613efcb81b7ae75f
Nathalie <98064725+natcua@users.noreply.github.com>
2022-03-23 17:58:35 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,3 +2,17 @@
 
 #1 - create bot (https://discord.com/developers/applications)
   #Name: Test1 Bot
+ 
+#2
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+	print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+	if message.author == client.user:
+		return
